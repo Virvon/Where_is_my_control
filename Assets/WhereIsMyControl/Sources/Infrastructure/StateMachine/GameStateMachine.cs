628d1824fa35591088ca783767876ae671e363c0
Даниил Константинов <dan130205@gmail.com>
2023-10-05 15:39:27 +0300

    Added game state machine

---
+++ Assets/WhereIsMyControl/Sources/Infrastructure/StateMachine/GameStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhereIsMyControl.Infrastructure
+{
+    public class GameStateMachine
+    {
+        private readonly Dictionary<Type, IState> _states;
+
+        private IState _currentState;
+
+        public GameStateMachine()
+        {
+            _states = new Dictionary<Type, IState>()
+            {
+                [typeof(BootstrapState)] = new BootstrapState()
+            };
+        }
+
+        public void Enter<TState>() where TState : IState
+        {
+            _currentState?.Exit();
+
+            IState state = _states[typeof(TState)];
+
+            _currentState = state;
+            _currentState.Enter();
+        }
+    }
+}
