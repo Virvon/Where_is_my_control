306c2b843b679ee691fb3698ecc8c044271ed1e6
llontores <80592083+llontores@users.noreply.github.com>
2023-10-07 17:23:16 +0600

    Better Menu

--- Assets/WhereIsMyControl/Sources/UI/Menu.cs
+++ Assets/WhereIsMyControl/Sources/UI/Menu.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private Button _closeButton;
+    [SerializeField] private Button _openButton;
     [SerializeField] private GameObject _screen;
 
     private void OnEnable()
     {
         _screen.SetActive(false);
+        _openButton.onClick.AddListener(Open);
         _closeButton.onClick.AddListener(Close);
     }
 
     private void OnDisable()
     {
+        _openButton.onClick.RemoveListener(Open);
         _closeButton.onClick.RemoveListener(Close);
     }
 
